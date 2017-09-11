c5914cd882e3f0ceda5bba7a7ac0ee64eee3012b
Miguel Montañez <cmiguelmg@gmail.com>
2017-09-11 08:15:12 -0500

    Update build-docker.sh
    Se actualizan argumentos

--- build-docker.sh
+++ build-docker.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
-cd web
-sh build-web.sh
-cd .. && docker-compose -d up
+# Number of arguments must be 1: backend or frontend
+if [$# -eq 1]
+then
+  if [#1 == 'backend']
+  then
+    docker-compose -d up mongo ks_api
+  fi
+  if [#1 == 'frontend']
+  then
+    cd web
+    sh build-web.sh
+    cd .. && docker-compose -d up ks_web
+  fi
+else
+  echo 'The script needs just 1 argument, it can be backend or frontend'
+fi
