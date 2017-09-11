f64e96fafecc3688a67d2a81e7f0e4300ce0d9b6
minmiguelm <cmiguelmg@gmail.com>
2017-09-11 15:05:05 -0500

    arreglo de script

--- build-docker.sh
+++ build-docker.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
 # Number of arguments must be 1: backend or frontend
-if [$# -eq 1]
+if [ $# == 1 ]
 then
-  if [#1 == 'backend']
+  if [ $1 == 'backend' ]
   then
     docker-compose -d up mongo ks_api
   fi
-  if [#1 == 'frontend']
+  if [ $1 == 'frontend' ]
   then
     cd web
     sh build-web.sh
     cd .. && docker-compose -d up ks_web
   fi
 else
-  echo 'The script needs just 1 argument, it can be backend or frontend'
+  echo 'The script needs an argument, it can be backend or frontend'
 fi
