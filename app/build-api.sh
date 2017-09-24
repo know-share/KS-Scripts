7636af239c45ecd12735b471f34cad87e9976ffc
minmiguelm <cmiguelmg@gmail.com>
2017-09-24 16:45:56 -0500

    cambio en scripts

--- app/build-api.sh
+++ app/build-api.sh
@@ -2,6 +2,7 @@
 
 rm -rf KS-API &&\
 git clone https://github.com/know-share/KS-API.git &&\
-cp application.properties KS-API/src/main/resources/
+cp application.properties KS-API/src/main/resources/ &&\
+cp keystore.p12 KS-API/src/main/resources/
 
 echo 'Build api successful'
--- web/build-web.sh
+++ web/build-web.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
-curl -LOk https://github.com/know-share/KS-Web/archive/master.zip &&\
-unzip master.zip &&\
-cd KS-Web-master &&\
+git clone https://github.com/know-share/KS-Web.git &&\
+cd KS-Web &&\
 cp ../constants.ts src/app/entities/constants.ts &&\
 npm install && npm run build:prod &&\
 mv dist/ ../ && cd .. &&\
-rm -rf KS-Web-master master.zip
+rm -rf KS-Web/
 
 # docker build -t ks_web web/ &&\
 # docker run -d -p 80:80 ks_web
